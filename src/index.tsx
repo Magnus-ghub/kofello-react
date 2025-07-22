7a3831b6621636d010d125b49eebc1387780cd9b
ilhom nomozov <nomozof@IlhomMacBook.local>
2025-07-23 02:15:07 +0900

    fix: MUI customized integration

--- src/index.tsx
+++ src/index.tsx
@@ -6,8 +6,9 @@ import App from "./app/App";
 import reportWebVitals from "./reportWebVitals";
 import  CssBaseline  from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/material";
-import theme from "./theme";
+import theme from "./app/MaterialTheme";
 import "./css/index.css";
+
 
 ReactDOM.render(
   <React.StrictMode>
