f176183e3a7c97caf05be6eedc9d9a696db7a0c7
ilhom nomozov <nomozof@IlhomMacBook.local>
2025-07-24 22:57:33 +0900

    R task bajarildi

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import { store } from "./app/store";
 import App from "./app/App";
 import reportWebVitals from "./reportWebVitals";
 import  CssBaseline  from "@mui/material/CssBaseline";
-import { ThemeProvider } from "@mui/material";
+import { ThemeProvider } from "@mui/material/styles";
 import theme from "./app/MaterialTheme";
 import "./css/index.css";
 import { BrowserRouter as Router } from "react-router-dom";
--- src/train.ts
+++ src/train.ts
@@ -1,4 +1,33 @@
 console.log("ts ishga tushti");
+// TASK R
+function calculate(sum: string): number {
+  const result = sum.split(" ");
+  const sum1 = Number(result[0]);
+  const operator = result[1];
+  const sum2 = Number(result[2]);
+  
+  if (operator === "+") return sum1 + sum2;
+  if (operator === "-") return sum1 - sum2;
+  if (operator === "*") return sum1 * sum2;
+  if (operator === "/") return sum1 / sum2;
+  return 0;
+
+}
+
+console.log(calculate("4 + 3"));
+console.log(calculate("15 * 3"));
+console.log(calculate("120 / 15"));
+
+
+/**
+Shunday function yozing, u string parametrga ega bo'lsin.
+Agar argument sifatida berilayotgan string, "1 + 2" bo'lsa,
+string ichidagi sonlarin yig'indisni hisoblab, number holatida qaytarsin
+
+MASALAN: calculate("1 + 3"); return 4;
+1 + 3 = 4, shu sababli 4 natijani qaytarmoqda.
+ */
+
 // TASK Q:
 function hasProperty(obj: object, key: string): boolean {
     return key in obj;
