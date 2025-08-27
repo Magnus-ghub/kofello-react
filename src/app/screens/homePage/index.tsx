85a744d3fe929f66b5f5a50f196efb9b49547b8a
ilhom nomozov <nomozof@IlhomMacBook.local>
2025-08-27 13:59:20 +0900

    fix: retrieve fresh menu and modify NewDishes component

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -9,7 +9,7 @@ import "../../../css/home.css";
 
 import { useDispatch } from "react-redux";
 import { Dispatch } from "@reduxjs/toolkit";
-import { setPopularDishes } from "./slice";
+import { setNewDishes, setPopularDishes } from "./slice";
 import { Product } from "../../../lib/types/product";
 import ProductService from "../../services/ProductService";
 import { ProductCollection } from "../../../lib/enums/product.enum";
@@ -17,10 +17,11 @@ import { ProductCollection } from "../../../lib/enums/product.enum";
 /** REDUX SLICE & SELECTOR **/
 const actionDispatch = (dispatch: Dispatch) => ({
   setPopularDishes: (data: Product[]) => dispatch(setPopularDishes(data)),
+  setNewDishes: (data: Product[]) => dispatch(setNewDishes(data)),
 });
 
 export default function HomePage() {
-  const { setPopularDishes} = actionDispatch(useDispatch());
+  const { setPopularDishes, setNewDishes} = actionDispatch(useDispatch());
   
   useEffect(() => {
     // Backend server data fetch => Data
@@ -35,6 +36,21 @@ export default function HomePage() {
       setPopularDishes(data);
     })
     .catch((err) => console.log(err));
+
+    product
+      .getProducts({
+        page: 1,
+        limit: 4,
+        order: "createdAt",
+    })
+    .then((data) => {
+      setNewDishes(data);
+    })
+    .catch((err) => console.log(err));
+
+
+
+
   }, []);
 
   return (
